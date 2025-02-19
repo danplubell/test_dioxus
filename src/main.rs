861362df0843ca167513600ffbe01924b0f4171c
Dan Plubell <Dan.Plubell@coxautoinc.com>
2025-02-18 20:25:11 -0500

    initial update

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use dioxus::logger::tracing::{debug, Level};
+use dioxus::prelude::*;
+use log::log;
+
+fn main() {
+    dioxus::logger::init(Level::DEBUG).expect("logger failed to init");
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+
+    let mut count = use_signal(|| 0);
+    use_effect(move || {
+        // When we read count, it becomes a dependency of the effect
+        let current_count = count();
+        // Whenever count changes, the effect will rerun
+        debug!("{current_count}");
+    });
+
+    rsx! {
+        button { onclick: move |_| count += 1, "Increment" }
+
+        div { "Count is {count}" }
+    }
+}
+
